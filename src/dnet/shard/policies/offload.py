5fa6ea2721bdfa4999bd2194eaaf05d7451fc2f1
andthattoo <omer@firstbatch.xyz>
2025-11-25 18:41:27 +0300

    logprob propagation

--- src/dnet/shard/policies/offload.py
+++ src/dnet/shard/policies/offload.py
@@ -385,6 +385,8 @@ class OffloadPolicy(ComputePolicy):
                             dtype=str(self.runtime._wire_mx_dtype),
                             callback_url=msg.callback_url,
                             tensor=x_cast,
+                            req_logprobs=msg.req_logprobs,
+                            req_top_logprobs=msg.req_top_logprobs,
                         )
 
                     self.runtime.emit_result(output_msg)
